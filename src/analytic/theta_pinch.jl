696976f2d37ff387faa0bc5c3dd886f536621c88
Michael Kraus <michael.kraus@ipp.mpg.de>
2019-04-11 13:58:01 +0200

    Add description to theta-pinch equilibrium.

--- src/analytic/theta_pinch.jl
+++ src/analytic/theta_pinch.jl
@@ -1,5 +1,13 @@
-"""
-θ-pinch equilibrium in (x,y,z) coordinates.
+@doc raw"""
+θ-pinch equilibrium in (x,y,z) coordinates with covariant components of
+the vector potential given by
+```math
+A (x,y,z) = \frac{B_0}{2} \big( - y , \, x , \, 0 \big)^T
+```
+resulting in the magnetic field with covariant components
+```math
+B (x,y,z) = \big( 0 , \, 0 , \, B_0 \big)^T .
+```
 
 Parameters:
     B₀: B-field at magnetic axis
