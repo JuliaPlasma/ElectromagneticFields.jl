eae9db8b1d07df923d294987afcd3e01995a3714
Michael Kraus <michael.kraus@ipp.mpg.de>
2020-10-02 15:32:54 +0200

    Fix in ABC docstring.

--- src/analytic/abc.jl
+++ src/analytic/abc.jl
@@ -1,5 +1,5 @@
 @doc raw"""
-ABCEquilibrium equilibrium in (x,y,z) coordinates with covariant components of the vector
+Arnold-Beltrami-Childress (ABC) field in (x,y,z) coordinates with covariant components of the vector
 potential given by
 ```math
 A (x,y,z) = \big( a \, \sin(z) + c \, \cos(y) , \, b \, \sin(x) + a \, \cos(z) , \, c \, \sin(y) + b \, \cos(x) \big)^T
